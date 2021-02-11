6da16091f2bbc52425c5d73dc34dde20982924ba
vitor-frnds <78027852+vitor-frnds@users.noreply.github.com>
2021-02-10 22:17:30 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+
+#include "IntEstatistica.h"
+
+using namespace std;
+
+int main()
+{
+    int tam;
+
+    cout << "Determine o tamanho máximo da lista: ";
+    cin >> tam;
+
+    intEstatistica lista1(tam);      /// criando um objeto intEstatistica
+
+
+    return 0;
+}   
